da7221d104249dc4298a96b4b4459b960acb00ce
suriya k <suriya.kodieswaran@gmail.com>
2025-04-04 16:24:08 +0530

    APPROVAL FUNCTION

--- src/main/java/com/bornfire/controller/LoanOrginationController.java
+++ src/main/java/com/bornfire/controller/LoanOrginationController.java
@@ -1,6 +1,11 @@
 package com.bornfire.controller;
 
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,13 +16,21 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bornfire.entities.BLMS_PERSONALDET_REPO;
 import com.bornfire.entities.BLMS_PERSONAL_LOAN_ENTITY;
+import com.bornfire.entities.Chart_Acc_Entity;
 import com.bornfire.entities.CustomerRequest;
+import com.bornfire.entities.DMD_TABLE;
+import com.bornfire.entities.DepositEntity;
+import com.bornfire.entities.Lease_Loan_Work_Entity;
+import com.bornfire.entities.TRAN_MAIN_TRM_WRK_ENTITY;
+import com.bornfire.entities.Td_defn_table;
 
 @Controller
 @ConfigurationProperties("default")
@@ -45,7 +58,62 @@ public class LoanOrginationController {
 		}
 		return "Loan_Origintation/RETAIL_LOANAPPROVAL";
 	}
+
+	@RequestMapping(value = "approvefuncloan", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public Map<String, String> approvefuncs(@RequestParam(required = false) String formmode,
+			@RequestParam(required = false) String ApprefNO,
+			HttpServletRequest rq, @RequestParam(required = false) String reject_remarks,
+			@RequestParam(required = false) String hold_remarks) {
+		String user = (String) rq.getSession().getAttribute("USERID");
+		String msg =  "";
+		String username = (String) rq.getSession().getAttribute("USERNAME");
+		SimpleDateFormat compDate = new SimpleDateFormat("dd-MM-yyyy");
+		String Date = compDate.format(new Date());
+		BLMS_PERSONAL_LOAN_ENTITY up = blms_PERSONALDET_REPO.getRefNo(ApprefNO);
+		if (formmode.equals("approveAcc")) {
+			up.setVerify_flg("Y");
+			up.setVerify_time(new Date());
+			up.setVerify_user(user);
 	
+			msg += "Reference No: " + ApprefNO + "<br>";
+			msg += "Approved Name: " + username + " <br>";
+			msg += "Approved Date: " + Date + " ";
+			
+		} else if (formmode.equals("Holdfin")) {
+			/*
+			 * up.setStatus("HOLD"); up.getCa_preferred_name(); SimpleDateFormat compDate =
+			 * new SimpleDateFormat("dd-MM-yyyy"); String ComplianceDate =
+			 * compDate.format(new Date()); up.setHold_remarks(hold_remarks);
+			 * up.setVerify_time(new Date()); up.setVerify_user(user);
+			 * bACP_CUS_PROFILE_REPO.save(up); msg = "Reference No: " + ApprefNO +
+			 * " has been moved to HOLD LIST ";
+			 */
+
+		} else if (formmode.equals("Reject_acc")) {
+			/*
+			 * CustomerRequest up = bACP_CUS_PROFILE_REPO.findByref_norec(ApprefNO, rec_no);
+			 * up.setStatus("REJECT"); up.getCa_preferred_name();
+			 * 
+			 * up.setVerify_time(new Date()); up.setVerify_user(user); SimpleDateFormat
+			 * compDate = new SimpleDateFormat("dd-MM-yyyy"); String ComplianceDate =
+			 * compDate.format(new Date()); up.setReject_remarks(reject_remarks);
+			 * bACP_CUS_PROFILE_REPO.save(up); msg = "Reference No: " + ApprefNO +
+			 * "  Rejected";
+			 */
+		}
+
+		else {
+
+			msg = "Error Occured!!!";
+		}
+
+		Map<String, String> response = new HashMap<>();
+		response.put("message", msg);
+		return response;
+
+	}
+
 	
 	
 }
