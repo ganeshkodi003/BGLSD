ad20361e76584f3ee79da3daa4b5cd1a966864aa
suriya k <suriya.kodieswaran@gmail.com>
2025-04-04 12:05:31 +0530

    updated screen

--- src/main/java/com/bornfire/controller/LoanOrginationController.java
+++ src/main/java/com/bornfire/controller/LoanOrginationController.java
@@ -1,22 +1,31 @@
 package com.bornfire.controller;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bornfire.entities.BLMS_PERSONALDET_REPO;
+import com.bornfire.entities.BLMS_PERSONAL_LOAN_ENTITY;
+import com.bornfire.entities.CustomerRequest;
 
 @Controller
 @ConfigurationProperties("default")
 public class LoanOrginationController {
 	private static final Logger logger = LoggerFactory.getLogger(CustOnboardinController.class);
+	
+	@Autowired
+	BLMS_PERSONALDET_REPO blms_PERSONALDET_REPO;
 	
 	
 	@RequestMapping(value = "RetailloanApproval", method = { RequestMethod.GET, RequestMethod.POST })
@@ -32,5 +41,24 @@ public class LoanOrginationController {
 			md.addAttribute("formmode", "ModifyHead");
 		} 
 		return "Loan_Origintation/APPROVAL";
+	}
+	
+	
+	/* SURIYA */
+	@GetMapping("AllApprovedPersonal")
+	public List<BLMS_PERSONAL_LOAN_ENTITY> AllApproved(@RequestParam(required = false) String tran_id) {
+		return blms_PERSONALDET_REPO.getApprovelist();
+	}
+
+	@GetMapping("ApprovedPersonal")
+	public List<BLMS_PERSONAL_LOAN_ENTITY> Approved(@RequestParam(required = false) String tran_id,
+			@RequestParam(required = false) String part_tran_id) {
+		return blms_PERSONALDET_REPO.getapproved();
+	}
+
+	@GetMapping("NotApprovedPersonal")
+	public List<BLMS_PERSONAL_LOAN_ENTITY> NotApproved(@RequestParam(required = false) String tran_id,
+			@RequestParam(required = false) String part_tran_id) {
+		return blms_PERSONALDET_REPO.getnotapproved();
 	}
 }
--- src/main/java/com/bornfire/entities/BLMS_PERSONALDET_REPO.java
+++ src/main/java/com/bornfire/entities/BLMS_PERSONALDET_REPO.java
@@ -1,5 +1,7 @@
 package com.bornfire.entities;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,13 @@ public interface BLMS_PERSONALDET_REPO extends JpaRepository<BLMS_PERSONAL_LOAN_
 
 	@Query(value = "select NEXT VALUE FOR BLMS_PERSONAL_SEQ AS nextval", nativeQuery = true)
 	String srlnum();
+	
+	@Query(value = "SELECT * FROM BLMS_PERSONAL_LOAN_DETAILS WHERE verify_flg IN ('Y', 'N')", nativeQuery = true)
+	List<BLMS_PERSONAL_LOAN_ENTITY> getApprovelist();
+	
+	@Query(value = "select * from BLMS_PERSONAL_LOAN_DETAILS  where verify_flg='Y' ", nativeQuery = true)
+	List<BLMS_PERSONAL_LOAN_ENTITY> getapproved();
+	
+	@Query(value = "select * from BLMS_PERSONAL_LOAN_DETAILS  where verify_flg = 'N' ", nativeQuery = true)
+	List<BLMS_PERSONAL_LOAN_ENTITY> getnotapproved();
 }
